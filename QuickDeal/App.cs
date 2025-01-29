b5ac6a490a6c047fe6ad686862548779ac6a95f3
danielsludanov <dsludanov@gmail.com>
2025-01-29 15:25:07 +0300

    back-end auth

---
+++ QuickDeal/App.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace QuickDeal
+{
+    public partial class App : Application
+    {
+        public int CurrentUserID { get; set; }
+
+        public void SetCurrentID (int ID)
+        {
+            CurrentUserID = ID;
+        }
+    }
+}
--- QuickDeal/Authentication/Auth.xaml.cs
+++ QuickDeal/Authentication/Auth.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -16,10 +18,16 @@ namespace QuickDeal.Authentication
 {
     public partial class Auth : Window
     {
+        private readonly QuickDealEntities db;
+
+        private static readonly Regex LoginRegex = new Regex(@"^[a-zA-Z0-9]{6,}$");
+        private static readonly Regex PasswordRegex = new Regex(@"^[a-zA-Z0-9!@#$%^&*()_+\-=\[\]{}|;:'"",.<>?/]{6,}$");
+
         private bool isNav = false;
         public Auth()
         {
             InitializeComponent();
+            db = new QuickDealEntities();
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
@@ -41,6 +49,55 @@ namespace QuickDeal.Authentication
 
         private void BtnLogin_Click(object sender, RoutedEventArgs e)
         {
+            string Login = UserLogin.Text;
+            string Password = UserPassword.Password;
+
+            // Валидация данных, проверка записи на существование
+
+            if (!LoginRegex.IsMatch(Login))
+            {
+                MessageBox.Show("Вы неправильно ввели логин",
+                    "Ошибка",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+                MessageBox.Show("Логин может содержать: английские символы\n" +
+                    "Цифры" +
+                    "Минимальное количество символов в логине: 6",
+                    "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+
+            var CheckUser = db.users.AsNoTracking().FirstOrDefault(u => u.login == Login);
+
+            if (CheckUser == null)
+            {
+                MessageBox.Show("Пользователя с таким логином не существует в системе",
+                    "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (!PasswordRegex.IsMatch(Password))
+            {
+                MessageBox.Show("Вы неправильно ввели пароль",
+                    "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show("Пароль может содержать: английские символы\n" +
+                    "Цифры\n" +
+                    "Специальные символы" +
+                    "Минимальное колчество символов в пароле: 6",
+                    "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+
+               
+            }
+             ((App)Application.Current).CurrentUserID = CheckUser.user_id;
+            MessageBox.Show("Вы авторизовались!", 
+                "Информация", 
+                MessageBoxButton.OK, MessageBoxImage.Information);
+
+            isNav = true;
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
 
         }
 
