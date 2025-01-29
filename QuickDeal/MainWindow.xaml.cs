0ad00ad62041d53f6dd62e956cdd8533d94b6f41
danielsludanov <dsludanov@gmail.com>
2025-01-30 01:11:27 +0300

    TO-DO: FIX SCROLL

--- QuickDeal/MainWindow.xaml.cs
+++ QuickDeal/MainWindow.xaml.cs
@@ -17,9 +17,48 @@ namespace QuickDeal
 {
     public partial class MainWindow : Window
     {
+        private readonly QuickDealEntities db;
         public MainWindow()
         {
             InitializeComponent();
+            db = new QuickDealEntities();
+            ListAds.ItemsSource = db.ads.ToList();
+            LoadCities();
+            LoadCategories();
+            LoadTypes();
+            LoadStatuses();
+        }
+
+        private void LoadCities()
+        {
+            var cities = db.cities.ToList();
+            City.ItemsSource = cities;
+            City.DisplayMemberPath = "city_name";
+            City.SelectedValuePath = "city_id";
+        }
+
+        private void LoadCategories()
+        {
+            var categories = db.ad_categories.ToList();
+            Category.ItemsSource = categories;
+            Category.DisplayMemberPath = "ad_category_name";
+            Category.SelectedValuePath = "ad_category_id";
+        }
+
+        private void LoadTypes()
+        {
+            var types = db.ad_types.ToList();
+            Type.ItemsSource = types;
+            Type.DisplayMemberPath = "ad_type_name";
+            Type.SelectedValuePath = "ad_type_id";
+        }
+        
+        private void LoadStatuses()
+        {
+            var statuses = db.ad_statuses.ToList();
+            Status.ItemsSource = statuses;
+            Status.DisplayMemberPath = "ad_status_name";
+            Status.SelectedValuePath = "ad_status_id";
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
