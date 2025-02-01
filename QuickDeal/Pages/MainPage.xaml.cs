1d3e50b4053c69c260a54e9b433f3e63064d72b3
danielsludanov <dsludanov@gmail.com>
2025-02-01 12:10:20 +0300

    final

--- QuickDeal/Pages/MainPage.xaml.cs
+++ QuickDeal/Pages/MainPage.xaml.cs
@@ -1,24 +1,10 @@
 ﻿using QuickDeal.Authentication;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace QuickDeal.Pages
 {
-    /// <summary>
-    /// Логика взаимодействия для MainPage.xaml
-    /// </summary>
     public partial class MainPage : Page
     {
         private readonly QuickDealEntities db;
